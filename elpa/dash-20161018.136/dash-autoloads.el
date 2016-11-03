3bbabc356d8e382d4c485a8ceaad10364f63207a
Geoffrey Pursell <geoffp@gmail.com>
2016-11-03 13:16:03 -0500

    delete untracked files and add .gitignore

--- elpa/dash-20161018.136/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (22538 32241 847134 676000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
