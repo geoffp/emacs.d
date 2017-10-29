ba6843a6f752e67ed04253d7b30d732b9a205779
Geoffrey Pursell <geoffrey.pursell@target.com>
2017-10-29 17:09:30 -0500

    updates!

---
+++ elpa/vue-mode-20171029.1137/vue-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vue-mode" "20171029.1137" "Major mode for vue component based on mmm-mode" '((mmm-mode "0.5.4") (vue-html-mode "0.1") (ssass-mode "0.1") (edit-indirect "0.1.4")) :commit "dc60ef41410419a261f1388851e618b32ec2852a" :keywords '("languages"))
