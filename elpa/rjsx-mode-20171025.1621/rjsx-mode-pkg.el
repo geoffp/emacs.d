608162e649e2d15f27634082f64e01fb52c04598
Geoffrey Pursell <geoffrey.pursell@target.com>
2017-10-29 10:00:07 -0500

    Check in...everything. Latest changes plus all installed packages. I hope this is worth it...

---
+++ elpa/rjsx-mode-20171025.1621/rjsx-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rjsx-mode" "20171025.1621" "Real support for JSX" '((emacs "24.4") (js2-mode "20170504")) :commit "121173c7786b86b224450e025c2cea8d2af64838" :url "https://github.com/felipeochoa/rjsx-mode/" :keywords '("languages"))
--- init.el
+++ init.el
@@ -15,21 +15,36 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-ignore-list (quote ("node_modules" "*.map" "main.*.*")))
   '(ansi-color-faces-vector
      [default bold shadow italic underline bold bold-italic bold])
+ '(async-bytecomp-package-mode t)
+ '(bookmark-save-flag 1)
+ '(css-indent-offset 2)
  '(custom-enabled-themes (quote (solarized-dark)))
   '(custom-safe-themes
      (quote
        ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" "a800120841da457aa2f86b98fb9fd8df8ba682cebde033d7dbf8077c1b7d677a" "3fd0fda6c3842e59f3a307d01f105cce74e1981c6670bb17588557b4cebfe1a7" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(editorconfig-mode t)
+ '(fill-column 95)
+ '(flycheck-check-syntax-automatically (quote (save idle-change mode-enabled)))
+ '(flycheck-disabled-checkers (quote (javascript-standard)))
+ '(flycheck-idle-change-delay 1)
  '(global-auto-complete-mode t)
  '(global-flycheck-mode t)
  '(ido-mode (quote both) nil (ido))
+ '(js2-strict-missing-semi-warning nil)
+ '(mocha-options "--recursive --colors")
+ '(mocha-reporter "spec")
  '(ns-command-modifier (quote meta))
+ '(package-enable-at-startup nil)
   '(package-selected-packages
      (quote
-       (rainbow-mode expand-region js-doc auto-complete web-mode ag json-mode exec-path-from-shell flycheck markdown-mode jade editorconfig magit flx-ido projectile neotree js2-mode js2-refactor gruvbox-theme monokai-theme zenburn-theme spacegray-theme solarized-theme color-theme-solarized color-theme-sanityinc-tomorrow)))
+       (vue-mode iedit mocha mmm-mode nginx-mode swift-mode rjsx-mode yaml-mode rainbow-mode expand-region js-doc auto-complete web-mode ag json-mode exec-path-from-shell flycheck markdown-mode jade editorconfig magit flx-ido projectile neotree js2-mode js2-refactor gruvbox-theme monokai-theme zenburn-theme spacegray-theme solarized-theme color-theme-solarized color-theme-sanityinc-tomorrow)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+  '(rainbow-html-colors-major-mode-list
+     (quote
+       (html-mode css-mode php-mode nxml-mode xml-mode web-mode js2-mode rjsx-mode)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
@@ -57,7 +72,7 @@
 (global-set-key (kbd "M-o") 'other-window)
 
 ;; js2 mode by default
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
 
 
 ;; file types to open in web-mode
@@ -65,9 +80,9 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 
 ;; make .html underscore template work in web-mode
-(setq web-mode-engines-alist
-'(("underscore.js" . "\\.html\\'"))
-)
+(setq web-mode-content-types-alist
+  '(("underscore.js" . "\\.html\\'")
+     ("jsx"          . "\\.js[x]?\\'")))
 
 ;; js2-refactor-mode by default
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
@@ -110,6 +125,27 @@
   (if (file-readable-p f)
     (load-file f)))
 
+;; let's do flycheck for JS in web-mode
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; as long we're talking flycheck, let's teach it to find eslint in node_modules.
+;; ripped from https://emacs.stackexchange.com/a/21207
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+;; load up mmm-mode
+(require 'mmm-mode)
+
 
 (provide 'init)
 ;;; init.el ends here
+(put 'narrow-to-region 'disabled nil)
