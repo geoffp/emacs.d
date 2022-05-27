bba3f1e7cb491edd1152b799587100cfceb27450
Geoff Pursell <geoffp@gmail.com>
2022-05-27 17:05:29 -0500

    Move checked-in local.el file to an example filename for reference

--- init.local.el
+++
@@ -1,3 +0,0 @@
-;; (set-face-attribute 'default nil :font "Victor Mono Italic-16")
-(set-face-attribute 'default nil :font "Fira Code-16")
-;; (set-frame-font FONT nil t)
---
+++ init.local.example.el
@@ -0,0 +1,10 @@
+;;; init.local --- machine-local customizations
+;;; Commentary:
+;;; This is mostly just to have different font sizes across different machines.
+;; (set-face-attribute 'default nil :font "Victor Mono Italic-16")
+;;; Code:
+(set-face-attribute 'default nil :font "Fira Code-12")
+;; (set-frame-font FONT nil t)
+
+(provide 'init.local)
+;;; init.local.el ends here
